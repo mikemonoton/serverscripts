2edab0ae6a49a2c0ac71a7f9f8427620f2447a14
Mike Thomas <mikemonoton@users.noreply.github.com>
2014-08-13 16:09:08 +0200

    Create mysql_backup.sh

---
+++ mysql_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Script zum dumpen von MySql Datenbanken
+# Datum: 22.06.2011
+# Autor: Computerwerkstatt Glocksee Hannover
+# Version 0.1
+
+
+# Aktuelles Datum für Dateinamen 
+DATE=$(date +%Y-%m-%d-%H%M%S)
+
+# Passwörter für die DB
+PASSWD_DBNAME=streng_geheimes_passwort
+
+# Backup Datenbank "DBNAME"
+mysqldump --opt -Q -u dbuser -B dbname -p$PASSWD_DBNAME >/srv/backup/mysql_backup/DBNAME.bak.db.$DATE.sql
+
+exit $?
